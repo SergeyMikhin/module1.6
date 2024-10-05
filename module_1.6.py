14a8ba160b22598fa9593598baad339da8e1c67e
SergeyMikhin <akvoliahim@gmail.com>
2024-10-06 01:27:17 +0300

    Add files via upload

---
+++ module_1.6.py
@@ -0,0 +1,6 @@
+number_of_completed_homework_assignments = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+print(1.5 / 12)
+time_for_one_task  = 0,125
+print("Курс:",course_name,"всего задач:",number_of_completed_homework_assignments,"затрачено часов:",number_of_hours_spent,"среднее время выполнения:",time_for_one_task,'часа.' )
